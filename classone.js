a8fc714dafdff73ea0185f3fb002f061eb2312fd
yogaNgowda <yogangowda81@gmail.com>
2026-06-14 19:30:32 +0530

    react project 1st commit

---
+++ classone.js
@@ -0,0 +1,15 @@
+import{arr}from './practice.js'; // this is named export call exact name with {}
+
+import user from './practice.js' // this is default export we can call any name
+
+console.log(arr)
+
+console.log(user)
+
+
+
+
+// let h1=document.createElement('h1')
+// h1.innerHTML="hello from js"
+
+// document.body.appendChild(h1)
---
+++ practice.js
@@ -0,0 +1,5 @@
+export let arr=[1,2,3,4,5]
+
+let user="yoga"
+
+export default user;
